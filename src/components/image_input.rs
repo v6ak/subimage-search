74afa4d8a66e68e8d2cbb33d0f5a41dbbddc634d
Vít Šesták 'v6ak <you@example.com>
2025-03-09 14:08:04 +0100

    Rewrite dragover highlighting, fixes permanent dragover highlight

--- src/components/image_input.rs
+++ src/components/image_input.rs
@@ -3,54 +3,9 @@ use web_sys::{DragEvent, Event, HtmlElement};
 use web_sys::{FileList, HtmlInputElement};
 use yew::prelude::*;
 
-use wasm_bindgen::prelude::*;
-
 fn prevent_default(event: &Event) {
     event.prevent_default();
     event.stop_propagation();
-}
-
-fn setup_drag_handlers(element: &HtmlElement) {
-    let el = element.clone();
-    let dragenter = Closure::wrap(Box::new(move |e: Event| {
-        prevent_default(&e);
-        el.set_class_name("image-input dragover");
-    }) as Box<dyn FnMut(_)>);
-    element
-        .add_event_listener_with_callback("dragenter", dragenter.as_ref().unchecked_ref())
-        .unwrap();
-    dragenter.forget();
-
-    let el = element.clone();
-    let dragover = Closure::wrap(Box::new(move |e: Event| {
-        prevent_default(&e);
-        el.set_class_name("image-input dragover");
-    }) as Box<dyn FnMut(_)>);
-    element
-        .add_event_listener_with_callback("dragover", dragover.as_ref().unchecked_ref())
-        .unwrap();
-    dragover.forget();
-
-    let el = element.clone();
-    let dragleave = Closure::wrap(Box::new(move |e: Event| {
-        prevent_default(&e);
-        el.set_class_name("image-input");
-    }) as Box<dyn FnMut(_)>);
-    element
-        .add_event_listener_with_callback("dragleave", dragleave.as_ref().unchecked_ref())
-        .unwrap();
-    dragleave.forget();
-}
-
-fn setup_drag_blocking_handlers(element: &HtmlElement) {
-    let handler = Closure::wrap(Box::new(|e: Event| {
-        prevent_default(&e);
-    }) as Box<dyn FnMut(_)>);
-    for event in ["dragenter", "dragover", "dragleave", "drop"].iter() {
-        element
-            .add_event_listener_with_callback(event, handler.as_ref().unchecked_ref())
-            .unwrap();
-    }
 }
 
 #[derive(Properties, PartialEq)]
@@ -75,43 +30,64 @@ fn on_change(cb: Callback<FileList>) -> Callback<Event> {
     })
 }
 
-#[function_component(ImageInput)]
-pub fn image_input(props: &ImageInputProps) -> Html {
-    let node_ref = use_node_ref();
-
-    // Setup drag & drop handlers
-    {
-        let node_ref = node_ref.clone();
-        let disabled = props.disabled;
-        use_effect_with((), move |_| {
-            if let Some(element) = node_ref.cast::<HtmlElement>() {
-                if disabled {
-                    setup_drag_blocking_handlers(&element);
-                } else {
-                    setup_drag_handlers(&element);
-                }
-            }
-            || ()
-        });
-    }
-
-    // Handle drop event
-    let ondrop = if props.disabled {
+fn on_drop(props: &ImageInputProps, label_ref: &NodeRef) -> Callback<DragEvent> {
+    if props.disabled {
         Callback::from(|event: DragEvent| {
             prevent_default(&event);
         })
     } else {
         let on_upload = props.on_upload.clone();
+        let label_ref = label_ref.clone();
         Callback::from(move |event: DragEvent| {
             prevent_default(&event);
+            node_ref_toggle_class(&label_ref, "dragover", false);
             if let Some(files) = event.data_transfer().and_then(|x| x.files()) {
                 on_upload.emit(files.clone());
             }
         })
-    };
+    }
+}
 
+fn on_drag_leave(label_ref: &NodeRef) -> Callback<DragEvent> {
+    let label_ref = label_ref.clone();
+    Callback::from(move |event: DragEvent| {
+        prevent_default(&event);
+        node_ref_toggle_class(&label_ref, "dragover", false);
+    })
+}
+
+fn on_drag_start(props: &ImageInputProps, label_ref: &NodeRef) -> Callback<DragEvent> {
+    let disabled = props.disabled;
+    let label_ref = label_ref.clone();
+    Callback::from(move |event: DragEvent| {
+        prevent_default(&event);
+        if !disabled {
+            node_ref_toggle_class(&label_ref, "dragover", true);
+        }
+    })
+}
+
+fn node_ref_toggle_class(node_ref: &NodeRef, class: &str, add: bool) {
+    if let Some(element) = node_ref.cast::<HtmlElement>() {
+        element.class_list().toggle_with_force(class, add).unwrap();
+    } else {
+        panic!("Failed to cast element to HtmlElement: {:?}", node_ref);
+    }
+}
+
+#[function_component(ImageInput)]
+pub fn image_input(props: &ImageInputProps) -> Html {
+    let label_ref = use_node_ref();
     html! {
-        <label class="image-input" id={format!("{}-container", props.input_id)} ref={node_ref} {ondrop}>
+        <label
+            ref={&label_ref}
+            class={if props.disabled {"image-input disabled"} else {"image-input"}}
+            id={format!("{}-container", props.input_id)}
+            ondrop={on_drop(props, &label_ref)}
+            ondragenter={on_drag_start(props, &label_ref)}
+            ondragover={on_drag_start(props, &label_ref)}
+            ondragleave={on_drag_leave(&label_ref)}
+        >
             <h3>{&props.label}</h3>
             <input
                 type="file"
